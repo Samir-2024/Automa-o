2c9fee32ed9426ec581833bb0863def6f4f150b8
Samir <samir.chehade42@gmail.com>
2024-10-13 13:59:07 -0300

    Update automacao.py

--- automacao.py
+++ automacao.py
@@ -3,32 +3,38 @@ import pywhatkit
 
 # Inicializar o reconhecedor de fala
 rec = sr.Recognizer()
+while True:
+    decisao = input("Deseja Continuar [s] ou [n]: ")
+    
+    if decisao == 'n':
+        break
+    
+    if decisao == 's':
+        print("Pode falar:")
+        with sr.Microphone() as mic:
+            audio = rec.listen(mic)
+            try:
+                t1 = rec.recognize_google(audio, language="pt-BR")
+                print("Você disse: " + t1)
+            except sr.UnknownValueError:
+                print("Não consegui entender o que foi dito")
 
-print("Pode falar:")
-with sr.Microphone() as mic:
-    audio = rec.listen(mic)
-    try:
-        t1 = rec.recognize_google(audio, language="pt-BR")
-        print("Você disse: " + t1)
-    except sr.UnknownValueError:
-        print("Não consegui entender o que foi dito")
+            except sr.RequestError:
+                print("Erro ao se comunicar com o serviço de reconhecimento de fala")
 
-    except sr.RequestError:
-        print("Erro ao se comunicar com o serviço de reconhecimento de fala")
-
-    if 'tocar música' in t1 :
-        print("Qual música você quer ouvir?")
-        try:
-            audio1 = rec.listen(mic)
-            t2 = rec.recognize_google(audio1, language="pt-BR")
-            print("Tocando:", t2)
-            pywhatkit.playonyt(t2)
+            if 'tocar música' in t1 :
+                print("Qual música você quer ouvir?")
+                try:
+                    audio1 = rec.listen(mic)
+                    t2 = rec.recognize_google(audio1, language="pt-BR")
+                    print("Tocando:", t2)
+                    pywhatkit.playonyt(t2)
             
-        except sr.UnknownValueError:
-            print("Não consegui entender o que você disse")
+                except sr.UnknownValueError:
+                    print("Não consegui entender o que você disse")
 
-    if 'enviar mensagem' in t1 or 'mensagem' in t1:
-        from datetimee import enviar_mensagem_user
+            if 'Enviar mensagem' in t1 or 'mensagem' in t1:
+                from datetimee import enviar_mensagem_user
         
-    else:
-        pass
+            else:
+                pass
